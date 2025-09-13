a54eee44b4db6bc7efd82129d6b615e3dc90b1f1
Pradeep <machapradeep478@gmail.com>
2025-09-13 21:26:14 +0530

    connected to mongo db atlas

--- server/config/db.js
+++ server/config/db.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    await mongoose.connect('mongodb://localhost:27017/Task_Manager')
+    await mongoose.connect('mongodb+srv://Pradeep:119131Pradeep@cluster0.sckuy.mongodb.net/Task_Manager?retryWrites=true&w=majority')
         .then(() => console.log("Connected to Task_Manager successfully"))
 }
